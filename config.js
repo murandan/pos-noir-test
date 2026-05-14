b599fa0f3848f471b953a07ef55970f9f0542d19
muratanel <muratanel7@gmail.com>
2026-05-14 11:08:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 // Файл глобальных настроек (config.js)
 
-// 1. Вставьте сюда ваш актуальный ENCODED текст для Google Apps Script
-const _0x1a8f = "https%3A%2F%2Fscript.google.com%2Fmacros%2Fs%2FAKfycbw9j0r3gQJRgZ2N50-NhNeFZBIBJP5dubSEdnYk2FllU-0hEz1YWQdKt6DykctRyMP5%2Fexec"; 
-
-// 2. Ссылка на вебхук Make.com
-const _0x5bc2 = "aHR0cHM6Ly9ob29rLmV1MS5tYWtlLmNvbS85dWQyeWV0dHIzbDZrdG81bmx0cXR0am9jZmI2cDZvZw==";
-
-// Расшифровка переменных (эти переменные станут доступны всем HTML-файлам)
-const APPS_SCRIPT_URL = window.atob(_0x1a8f);
-const MAKE_WEBHOOK_URL = window.atob(_0x5bc2);
+// 1. Вставьте сюда ваш актуальную ссылку для Google Apps Script
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw9j0r3gQJRgZ2N50-NhNeFZBIBJP5dubSEdnYk2FllU-0hEz1YWQdKt6DykctRyMP5/exec"; 
